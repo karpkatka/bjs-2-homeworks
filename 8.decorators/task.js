70d39c966774d8687132ebaa3cae4e290a537f0a
Ekaterina Bakaeva <bakaeva.es@gmail.com>
2023-05-01 11:42:46 +0300

    8 задание выполнено

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,51 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-  
+	let cache = [];
+
+	function wrapper(...args) {
+		let hash = md5(args); //почему тут без spread-оператора?
+		let objectInCache = cache.find(item => item.hash === hash)
+		if (objectInCache) {
+			console.log("Из кэша: " + objectInCache.value);
+			return "Из кэша: " + objectInCache.value;
+		}
+
+		let result = func(...args);
+		cache.push({
+			hash,
+			value: result
+		});
+		if (cache.length > 5) {
+			cache.shift();
+		}
+		console.log("Вычисляем: " + result);
+		return "Вычисляем: " + result;
+	}
+	return wrapper
 }
+
+
+
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
-  
+	let timeoutID;
+
+	function wrapper(...args) {
+		if (!timeoutID) {
+			wrapper.count += 1;
+			func(args);
+		} else {
+			clearTimeout(timeoutID);
+		}
+		timeoutID = setTimeout(() => {
+			wrapper.count += 1;
+			func(...args);
+		}, delay)
+		wrapper.allCount += 1;
+	}
+
+	wrapper.allCount = Number();
+	wrapper.count = Number();
+	return wrapper;
 }
